9a5ae7d9eeb2975009600a055558e29df1d528bb
Diego Estrada <est.diego03@gmail.com>
2020-11-12 19:04:33 -0600

    can move and display fps

--- cast.py
+++ cast.py
@@ -1,224 +1,270 @@
+import os 
 import pygame
 from math import pi, cos, sin, atan2
 
-
-BLACK = (0, 0, 0)
+#Variables del personaje y el mundo
+screenSize = 200
+zoom = 70
+initialX = 20
+initialY = 20
+blockSize = 50
+fov = pi/3
+a = 0
+back = 0.1
+spriteSize = 256
+characterSize = 32
+characterRes = 102
+CLOCK    = pygame.time.Clock()
+FPS      = 60000
 WHITE = (255, 255, 255)
-BACKGROUND = (0, 255, 255)
+#Cargas de imagenes de texturas
+wall1 = pygame.image.load('./sprites/wall1.png')
+wall2 = pygame.image.load('./sprites/wall2.png')
 
 
-wall1 = pygame.image.load('./wall1.png')
-wall2 = pygame.image.load('./wall2.png')
-wall3 = pygame.image.load('./wall3.png')
-wall4 = pygame.image.load('./wall4.png')
-wall5 = pygame.image.load('./wall5.png')
-
+#Asignacion de imagenes en un diccionario
 textures = {
   "1": wall1,
-  "2": wall2,
-  "3": wall3,
-  "4": wall4,
-  "5": wall5,
+  "2": wall2
 }
 
-hand = pygame.image.load('./player.png')
+#Carga de imagenes de la mano
+hand = pygame.image.load('./sprites/hand.png')
 
+#Carga de imagenes de sprites en sus posiciones en un arreglo de diccionarios (10 ladies)
 enemies = [
   {
     "x": 100,
     "y": 200,
-    "texture": pygame.image.load('./sprite2.png')
+    "texture": pygame.image.load('./sprites/gem1.png')
   },
   {
     "x": 280,
     "y": 190,
-    "texture": pygame.image.load('./sprite3.png')
+    "texture": pygame.image.load('./sprites/gem2.png')
   },
   {
     "x": 225,
     "y": 340,
-    "texture": pygame.image.load('./sprite4.png')
+    "texture": pygame.image.load('./sprites/gem3.png')
   },
   {
     "x": 220,
     "y": 425,
-    "texture": pygame.image.load('./sprite1.png')
+    "texture": pygame.image.load('./sprites/gem4.png')
   },
   {
     "x": 320,
     "y": 420,
-    "texture": pygame.image.load('./sprite2.png')
+    "texture": pygame.image.load('./sprites/gem5.png')
   }
 ]
+  
+
 
 class Raycaster(object):
   def __init__(self, screen):
     _, _, self.width, self.height = screen.get_rect()
     self.screen = screen
-    self.blocksize = 50
+    self.blocksize = blockSize
     self.player = {
-      "x": self.blocksize + 20,
-      "y": self.blocksize + 20,
-      "a": pi/3,
-      "fov": pi/3
+      "x": self.blocksize + initialX,
+      "y": self.blocksize + initialY,
+      "a": a,
+      "fov": fov
     }
     self.map = []
-    self.zbuffer = [-float('inf') for z in range(0, 500)]
-    # self.clear()
+    self.zbuffer = [-float('inf') for _ in range(0,200)]
+
 
   def clear(self):
-    for x in range(self.width):
-      for y in range(self.height):
-        r = int((x/self.width)*255) if x/self.width < 1 else 1
-        g = int((y/self.height)*255) if y/self.height < 1 else 1
-        b = 0
-        color = (r, g, b)
-        self.point(x, y, color)
+    self.screen.fill((back,back,back))
+
 
   def point(self, x, y, c = None):
     screen.set_at((x, y), c)
 
-  def draw_rectangle(self, x, y, texture):
-    for cx in range(x, x + 50):
-      for cy in range(y, y + 50):
-        tx = int((cx - x)*128 / 50)
-        ty = int((cy - y)*128 / 50)
-        c = texture.get_at((tx, ty))
-        self.point(cx, cy, c)
 
   def load_map(self, filename):
     with open(filename) as f:
       for line in f.readlines():
         self.map.append(list(line))
 
+ 
   def cast_ray(self, a):
     d = 0
     while True:
       x = self.player["x"] + d*cos(a)
       y = self.player["y"] + d*sin(a)
 
-      i = int(x/50)
-      j = int(y/50)
+      i = int(int(x)/blockSize)
+      j = int(int(y)/blockSize)
 
       if self.map[j][i] != ' ':
-        hitx = x - i*50
-        hity = y - j*50
+        hitx = x - i*blockSize
+        hity = y - j*blockSize
 
         if 1 < hitx < 49:
           maxhit = hitx
         else:
           maxhit = hity
 
-        tx = int(maxhit * 128 / 50)
-
+        tx = int(maxhit * spriteSize/blockSize)
         return d, self.map[j][i], tx
-
-      self.point(int(x), int(y), (255, 255, 255))
 
       d += 1
 
+
   def draw_stake(self, x, h, texture, tx):
-    start = int(250 - h/2)
-    end = int(250 + h/2)
+    start = int(screenSize/2 - h/2)
+    end = int(screenSize/2 + h/2)
     for y in range(start, end):
-      ty = int(((y - start)*128)/(end - start))
-      c = texture.get_at((tx, ty))
+      ty = int((y - start) * spriteSize/(end - start))
+      c = texture.get_at((tx,ty))
       self.point(x, y, c)
 
+
   def draw_sprite(self, sprite):
-    sprite_a = atan2(sprite["y"] - self.player["y"], sprite["x"] - self.player["x"])   # why atan2? https://stackoverflow.com/a/12011762
-
-    sprite_d = ((self.player["x"] - sprite["x"])**2 + (self.player["y"] - sprite["y"])**2)**0.5
-    sprite_size = (500/sprite_d) * 70
-
-    sprite_x = 500 + (sprite_a - self.player["a"])*500/self.player["fov"] + 250 - sprite_size/2
-    sprite_y = 250 - sprite_size/2
+    sprite_a = atan2(sprite["y"] - self.player["y"], sprite["x"] - self.player["x"])
+    sprite_d = ((self.player["x"] - sprite["x"])**2 + (self.player["y"] - sprite["y"])**2)**(1/2)
+    sprite_size = (screenSize/sprite_d) * zoom
+    sprite_x = (screenSize*(sprite_a - self.player["a"])/self.player["fov"] + screenSize/2 - sprite_size/2)
+    sprite_y = (screenSize/2 - sprite_size/2)
 
     sprite_x = int(sprite_x)
     sprite_y = int(sprite_y)
     sprite_size = int(sprite_size)
 
+    
     for x in range(sprite_x, sprite_x + sprite_size):
       for y in range(sprite_y, sprite_y + sprite_size):
-        if 500 < x < 1000 and self.zbuffer[x - 500] >= sprite_d:
-          tx = int((x - sprite_x) * 128/sprite_size)
-          ty = int((y - sprite_y) * 128/sprite_size)
+        if 0 < x < screenSize and self.zbuffer[x] >= sprite_d:
+          tx = int((x - sprite_x) * spriteSize/sprite_size)
+          ty = int((y - sprite_y) * spriteSize/sprite_size)
           c = sprite["texture"].get_at((tx, ty))
-          if c != (152, 0, 136, 255):
-            self.point(x, y, c)
-            self.zbuffer[x - 500] = sprite_d
 
-  def draw_player(self, xi, yi, w = 256, h = 256):
+          if   c != (152, 0, 136, 255):
+            self.point(x, y, c)
+            self.zbuffer[x] = sprite_d
+
+
+  def draw_player(self, xi, yi, w = characterRes, h = characterRes):
     for x in range(xi, xi + w):
       for y in range(yi, yi + h):
-        tx = int((x - xi) * 32/w)
-        ty = int((y - yi) * 32/h)
+        tx = int((x - xi) * characterSize/w)
+        ty = int((y - yi) * characterSize/h)
         c = hand.get_at((tx, ty))
         if c != (152, 0, 136, 255):
           self.point(x, y, c)
 
   def render(self):
-    for x in range(0, 500, 50):
-      for y in range(0, 500, 50):
-        i = int(x/50)
-        j = int(y/50)
-        if self.map[j][i] != ' ':
-          self.draw_rectangle(x, y, textures[self.map[j][i]])
-
-    self.point(self.player["x"], self.player["y"], (255, 255, 255))
-
-    for i in range(0, 500):
-      self.point(500, i, (0, 0, 0))
-      self.point(501, i, (0, 0, 0))
-      self.point(499, i, (0, 0, 0))
-
-    for i in range(0, 500):
-      a =  self.player["a"] - self.player["fov"]/2 + self.player["fov"]*i/500
+    for i in range(0, screenSize):
+      a =  self.player["a"] - self.player["fov"]/2 + self.player["fov"]*i/screenSize
       d, c, tx = self.cast_ray(a)
-      x = 500 + i
-      h = 500/(d*cos(a-self.player["a"])) * 70
+      x = i
+      h = screenSize/(d*cos(a-self.player["a"])) * zoom
       self.draw_stake(x, h, textures[c], tx)
+
       self.zbuffer[i] = d
 
+    #enemigos
     for enemy in enemies:
       self.point(enemy["x"], enemy["y"], (0, 0, 0))
       self.draw_sprite(enemy)
 
-    self.draw_player(1000 - 256 - 128, 500 - 256)
+    #jugador
+    self.draw_player(screenSize - characterRes - 50, screenSize - characterRes)
+
+def show_fps(clock,screen):
+    show_fps = "FPS: " + str(int(clock.get_fps()))
+    font = pygame.font.SysFont('Arial', 10, True)
+    render = font.render(show_fps,0,(255,255,255))
+    screen.blit(render, (150,5))
+
+def game(r):
+
+  gameOver = True
+  while gameOver:
+    r.clear()
+
+    try:
+
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE):
+                gameOver = False
+
+            
+            #Rotacion
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_a:
+                    r.player["a"] -= pi/10
+                    if r.player["a"] <= -11*pi/10:
+                        r.player["a"] = 9*pi/10
+                    else:
+                        pass
+            
+                elif e.key == pygame.K_d:
+                    r.player["a"] += pi/10
+                    if r.player["a"] >= 11*pi/10:
+                        r.player["a"] = -9*pi/10
+                    else:
+                        pass
+
+                #Direccion
+                elif e.key == pygame.K_RIGHT:
+                    r.player["y"] += 10*cos(r.player["a"])
+                    r.player["x"] += -1*10*sin(r.player["a"])
+                    if r.player["y"] <= 65 or r.player["y"] >= 630 or r.player["x"] <= 65 or r.player["x"] >= 1430:
+                        r.player["y"] -= 10*cos(r.player["a"])
+                        r.player["x"] -= -1*10*sin(r.player["a"])
+                    
+                elif e.key == pygame.K_LEFT:
+                    r.player["y"] += -1*10*cos(r.player["a"])
+                    r.player["x"] += 1*10*sin(r.player["a"])
+                    if r.player["y"] <= 65 or r.player["y"] >= 630 or r.player["x"] <= 65 or r.player["x"] >= 1430:
+                        r.player["y"] -= -1*10*cos(r.player["a"])
+                        r.player["x"] -= 1*10*sin(r.player["a"])
+
+                elif e.key == pygame.K_UP:
+                    r.player["x"] += 10*cos(r.player["a"])
+                    r.player["y"] += 10*sin(r.player["a"])
+                    if r.player["y"] <= 65 or r.player["y"] >= 630 or r.player["x"] <= 65 or r.player["x"] >= 1430:
+                        r.player["x"] -= 10*cos(r.player["a"])
+                        r.player["y"] -= 10*sin(r.player["a"])
+                
+
+                elif e.key == pygame.K_DOWN:
+                    r.player["x"] += -1*10*cos(r.player["a"])
+                    r.player["y"] += -1*10*sin(r.player["a"])
+                    if r.player["y"] <= 65 or r.player["y"] >= 630 or r.player["x"] <= 65 or r.player["x"] >= 1430:
+                        r.player["x"] -= -1*10*cos(r.player["a"])
+                        r.player["y"] -= -1*10*sin(r.player["a"])
+                            
+                
+                if e.key == pygame.K_f:
+                    if screen.get_flags() and pygame.FULLSCREEN:
+                        pygame.display.set_mode((screenSize, screenSize))
+                    else:
+                        pygame.display.set_mode((screenSize, screenSize),  pygame.DOUBLEBUF|pygame.HWACCEL|pygame.FULLSCREEN|pygame.HWSURFACE)
+
+      
+        r.render()    
+        show_fps(clock,screen)       
+        pygame.display.flip()
+        clock.tick(15) 
+        
+    except ZeroDivisionError:
+        pass
 
 pygame.init()
-screen = pygame.display.set_mode((1000, 500), pygame.DOUBLEBUF|pygame.HWACCEL|pygame.FULLSCREEN|pygame.HWSURFACE)
+
+
+screen = pygame.display.set_mode((screenSize, screenSize),pygame.FULLSCREEN | pygame.HWSURFACE | pygame.DOUBLEBUF | pygame.HWACCEL)
 screen.set_alpha(None)
 r = Raycaster(screen)
 r.load_map('./map.txt')
+clock = pygame.time.Clock()
+game(r)
 
-c = 0
-while True:
-  screen.fill((113, 113, 113))
-  r.render()
 
-  for e in pygame.event.get():
-    if e.type == pygame.QUIT or (e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE):
-      exit(0)
-    if e.type == pygame.KEYDOWN:
-      if e.key == pygame.K_a:
-        r.player["a"] -= pi/10
-      elif e.key == pygame.K_d:
-        r.player["a"] += pi/10
 
-      elif e.key == pygame.K_RIGHT:
-        r.player["x"] += 10
-      elif e.key == pygame.K_LEFT:
-        r.player["x"] -= 10
-      elif e.key == pygame.K_UP:
-        r.player["y"] += 10
-      elif e.key == pygame.K_DOWN:
-        r.player["y"] -= 10
-
-      if e.key == pygame.K_f:
-        if screen.get_flags() and pygame.FULLSCREEN:
-            pygame.display.set_mode((1000, 500))
-        else:
-            pygame.display.set_mode((1000, 500),  pygame.DOUBLEBUF|pygame.HWACCEL|pygame.FULLSCREEN)
-
-  pygame.display.flip()
